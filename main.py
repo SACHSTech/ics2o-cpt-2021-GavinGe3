f99596a7efd3e4eae7907ee22107fef537067d2b
GavinGe3 <gavin.ge23@ycdsbk12.ca>
2021-04-03 16:05:00 -0400

    Commit 4

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ for i in range(13):
     x = 270 + i*60
     y = 420
     buttons.append([x, y, chr(A + i + 13)])
-
+print(buttons)
 #game variable
 ram = 4
 flowers = 0 
@@ -52,7 +52,7 @@ questions_list = ["A circuit board that contains all principle components of the
 "Short for malicious software, this word describes harmful    computer programs",
 "This Operating System was created in 1985 by Microsoft",
 "Released in 1984 by Apple, this computer was the first       successful PC that featured the GUI",
-"This Operating System first released in 1991 runs on all    Android phones",
+"This Operating System first released in 1991 runs on all     Android phones",
 "The Operating System which macOS is based on was released in 1971"
 ]
 words_list = ["MOTHERBOARD", "CPU", "GPU", "RAM", "PSU", "MALWARE", "WINDOWS", "MACINTOSH", "LINUX", "UNIX"]
@@ -68,6 +68,7 @@ ramY = 630
 textOne = titleFont.render("Computer RAM Man", True, WHITE)
 textTwo = font.render("Easy", True, WHITE)
 textThree = font.render("Hard", True, WHITE)
+textFour = font.render("Instructions", True, WHITE)
 #global variable
 clock = pygame.time.Clock()
 
@@ -89,8 +90,7 @@ backgroundFour = pygame.transform.scale(backgroundFour, (1280,720))
 starImage = pygame.image.load("star.png").convert()
 starImage = pygame.transform.scale(starImage, (80, 60))
 starImage.set_colorkey(BLACK)
-sansImage = pygame.image.load("sans.png").convert()
-pcmrImage = pygame.image.load("PCMR.png").convert()
+
 pcImage = pygame.image.load("PC.png").convert()
 pcImage = pygame.transform.scale(pcImage, (300,300))
 flowerImage = pygame.image.load("flower.png").convert()
@@ -103,7 +103,20 @@ gpuImage = pygame.transform.scale(gpuImage, (250, 370))
 cpuImage = pygame.image.load("CPU.png").convert()
 cpuImage.set_colorkey((32,31,30))
 cpu = pygame.transform.scale(cpuImage, (200, 200))
-
+homeImage = pygame.image.load("home.jpg").convert()
+homeImage = pygame.transform.scale(homeImage, (110, 90))
+homeImage.set_colorkey((254,254,254))
+instructionsImage = pygame.image.load("instructions.jpg").convert()
+instructionsImage = pygame.transform.scale(instructionsImage, (864, 486))
+instructionsImage.set_colorkey((33,33,33))
+chipMunkImage = pygame.image.load("chipmunk.png").convert()
+chipMunkImage.set_colorkey((32,31,30))
+spiderImage = pygame.image.load("spider.png").convert()
+spiderImage.set_colorkey((32,31,30))
+humanImage = pygame.image.load("human.png").convert()
+humanImage.set_colorkey((32,31,30))
+treeImage = pygame.image.load('tree.png').convert()
+treeImage.set_colorkey((32,31,30))
 class GameScene:
     def __init__(self):
         self.scene = "loading"
@@ -111,7 +124,10 @@ class GameScene:
     # Menu Scene
     def loading(self):
         global ram
-
+        global word_number
+        global question_number
+        global flowers
+        global gussedLetters
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -120,12 +136,28 @@ class GameScene:
                 mouse_x, mouse_y = pygame.mouse.get_pos()
                 press_easy = mouse_x > 427 and mouse_x < 854 and mouse_y > 360 and mouse_y < 410
                 press_hard = mouse_x > 427 and mouse_x < 854 and mouse_y > 450 and mouse_y < 500
+                press_instructions = mouse_x > 427 and mouse_x < 854 and mouse_y > 540 and mouse_y < 590
+                # If user presses instruction button then loads the instructions scene
+                if press_instructions:
+                    self.scene = "instructions"
+                # If user presses hard button then gives the user 4 sticks of ram
                 if press_easy:
                     self.scene = "maingame"
                     ram = 4
+                    word_number = 0
+                    question_number = 0
+                    flowers = 0
+                    guessedLetters = []
+                    winningCount = len(words_list)
+                # If user presses hard button then gives the user 2 sticks of ram
                 if press_hard:
                     self.scene = "maingame"
                     ram = 2
+                    word_number = 0
+                    question_number = 0
+                    flowers = 0
+                    guessedLetters = []
+                    winningCount = len(words_list)
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     self.scene = "failscreen"
@@ -139,16 +171,42 @@ class GameScene:
         
         #draw title, buttons, and button text
         screen.blit(textOne, (300, 125))
-        pygame.draw.rect(screen, GREY, (427, rectY, 427, 50))
-        pygame.draw.rect(screen, GREY, (427, 450, 427, 50))
+
+        for i in range(3):
+            pygame.draw.rect(screen, GREY, (427, 360 + 90 * i, 427, 50))
+            
+        
         screen.blit(textTwo, (600, 373))
         screen.blit(textThree, (600, 463))
-
+        screen.blit(textFour, (530, 553))
         
         pygame.display.flip()
     
-    # The scene that contains the central hangman game
+    #instructions scene
+    def instructions(self):
+        
+        # Draw instructions and game developer images
+        screen.fill(BLACK)
+        
+        screen.blit(humanImage, (100, 325))
+        screen.blit(chipMunkImage, (600, 325))
+        screen.blit(instructionsImage, (200, -100))
+        screen.blit(homeImage, (-5, 625))
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                if mouse_x > 5 and mouse_x < 95 and mouse_y > 624 and mouse_y < 710:
+                    self.scene = "loading"
+        
+        pygame.display.flip()
+    
+    # Main Game Scene
     def maingame(self):
+
+        #Use global variable
         global question_number
         global word_number
         global ram
@@ -156,8 +214,13 @@ class GameScene:
         global guessedLetters
         global words_list
         global winningCount
+        
         screen.fill(BLACK)
         
+
+        # draw home button
+        screen.blit(homeImage, (-5,625))
+
         #Draws the Questions
         if len(questions_list[question_number]) <= 60:
             question = font.render(questions_list[question_number], True, WHITE)
@@ -212,7 +275,7 @@ class GameScene:
             screen.blit(textQuestionNum, (220 + x * 90, 10))
             pygame.draw.rect(screen, GREY, (185 + x*90, 65, 90, 10))
         
-        
+        # Draws progress bar on top of screen
         if word_number >= 1:
             for x in range(word_number):
                 pygame.draw.rect(screen, BLACK, (185 + x*90, 0, 90, 65))
@@ -221,7 +284,6 @@ class GameScene:
                 pygame.draw.rect(screen, GREEN, (185 + x*90, 65, 90, 10))
         
         pygame.draw.rect(screen, BLUE, (185 + word_number*90, 65, 90, 10))
-
 
         #Moves on to next word if the word is guessed correctly
         if guessed_word == words_list[word_number]:
@@ -255,6 +317,9 @@ class GameScene:
                 pygame.quit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_x, mouse_y = pygame.mouse.get_pos()
+                if mouse_x > 5 and mouse_x < 95 and mouse_y > 624 and mouse_y < 710:
+                    self.scene = "loading"
+        
                 # Uses the distance formula to determine if their cursor is on the button and if pressed adds letter to guessed
                 for button in buttons:
                     x, y, letter = button
@@ -265,6 +330,8 @@ class GameScene:
                             ram -= 1
                             flowers += 1
             
+
+            # Developer Access
             if event.type == pygame.KEYDOWN:
                 if event.type == pygame.K_o:
                     word_number -= 1
@@ -273,32 +340,33 @@ class GameScene:
                     word_number += 1
                     question_number += 1
                  
-                
+
+            # Developer Access
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     self.scene = "failscreen"
         
         pygame.display.flip()
 
-    # failscreen
+    # failscreen scene
     def failscreen(self):
+        #use these global varibles 
         global startTime
         global word_number
         global question_number
         global flowers
         global guessedLetters
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
+
             #detects button press and starts a stopwatch
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_x, mouse_y = pygame.mouse.get_pos()
                 if mouse_x > 126 and mouse_x < 376 and mouse_y > 377 and mouse_y < 437:
                     startTime = pygame.time.get_ticks()
-                    word_number = 0
-                    question_number = 0
-                    guessedLetters = []
-                    flowers = 0
+                    
 
         
         # background image
@@ -318,6 +386,7 @@ class GameScene:
         percentage += secondsPassed * 10 // 1
         counter -= secondsPassed
 
+        # prints the percentage of the restart process
         textPercentage = font.render((str(percentage) + "% complete"), True, WHITE)
         if percentage > 0 and percentage < 101: 
             pygame.draw.rect(screen, BLUE, (125, 376, 300, 60))
@@ -336,43 +405,50 @@ class GameScene:
         global question_number
         global winningCount
         global flowers
+
+        screen.fill(BLACK)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
+            # Go back to titlescreen
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_x, mouse_y = pygame.mouse.get_pos()
                 pressAgain = mouse_x > 74 and mouse_x < 426 and mouse_y > 599 and mouse_y < 651
                 pressExit = mouse_x > 849 and mouse_x < 1201 and mouse_y > 599 and mouse_y < 651
                 if pressAgain:
                     self.scene = "loading"
-                    word_number = 0
-                    question_number = 0
-                    flowers = 0
-                    winningCount = len(words_list)
+                   
 
                 if pressExit:
                     pygame.quit()
 
-        screen.blit(background, (0,0))
-        victoryText = titleFont.render("You WON!", True, BLACK)
+        #draw tree
+        screen.blit(treeImage, (160, 100))
+        
+        #draw text "You won 10 times"
+        victoryText = titleFont.render("You WON!", True, WHITE)
         for i in range(5):
             screen.blit(victoryText, (80, 120 + i * 100))
         for i in range(5):
             screen.blit(victoryText, (875, 120 + i * 100))
         
+        # draw buttons and button text
         pygame.draw.rect(screen, GREY, (75, 600, 350, 50))
         pygame.draw.rect(screen, GREY, (850, 600, 350, 50))
         playAgain = font.render("Play Again", True, BLACK)
         exitText = font.render("Exit", True, BLACK)
-
         screen.blit(playAgain, (150, 610))
         screen.blit(exitText, (985, 610))
 
         pygame.display.flip()
     
+
+    # class method for updating game scene
     def update(self): 
         if self.scene == "loading":
             self.loading()
+        if self.scene == "instructions":
+            self.instructions()
         if self.scene == "maingame":
             self.maingame()
         if self.scene == "failscreen":
@@ -380,7 +456,6 @@ class GameScene:
         if self.scene == "victoryscreen":
             self.victoryscreen()
         
-
 game_scene = GameScene()
 
 #timer variable
